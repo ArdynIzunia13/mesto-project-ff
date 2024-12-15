fdb245d1a58acd897c4e8bedb3745a9b1db56f65
Lebowski <joker13504@gmail.com>
2024-12-15 23:37:04 +0300

    final

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+function createCard(cardData) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 // @todo: DOM узлы
+    cardElement.querySelector('.card__image').src = cardData.link
+    cardElement.querySelector('.card__image').alt = cardData.name
+    cardElement.querySelector('.card__title').textContent = cardData.name
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+    deleteButton.addEventListener('click', () => {
+      deleteCard(cardElement);
+    });
+  
+    return cardElement;
+  }
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+  const placesList = document.querySelector('.places__list');
+   // @todo: Функция создания карточки
+   // @todo: Вывести карточки на страницу
+    initialCards.forEach(cardData => {
+        const card = createCard(cardData);
+        placesList.append(card);
+      });
+  
+  // @todo: Функция удаления карточки
+  function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+  

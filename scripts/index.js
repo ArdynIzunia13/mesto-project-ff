c167814a8d82758d5f84c3ae501d2052e37617f9
Lebowski <joker13504@gmail.com>
2024-12-16 12:14:50 +0300

    finalver

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 // @todo: Темплейт карточки
-function createCard(cardData) {
+function createCard(cardData, callbackDeleteCard) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 // @todo: DOM узлы
+    cardElement.querySelector('.card__image').setAttribute('aria-label', 'hidden')
     cardElement.querySelector('.card__image').src = cardData.link
     cardElement.querySelector('.card__image').alt = cardData.name
     cardElement.querySelector('.card__title').textContent = cardData.name
@@ -10,22 +11,21 @@ function createCard(cardData) {
     const deleteButton = cardElement.querySelector('.card__delete-button');
 
     deleteButton.addEventListener('click', () => {
-      deleteCard(cardElement);
+      callbackDeleteCard(cardElement);
     });
   
     return cardElement;
   }
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
   const placesList = document.querySelector('.places__list');
    // @todo: Функция создания карточки
    // @todo: Вывести карточки на страницу
     initialCards.forEach(cardData => {
-        const card = createCard(cardData);
+        const card = createCard(cardData, deleteCard);
         placesList.append(card);
       });
-  
-  // @todo: Функция удаления карточки
-  function deleteCard(cardElement) {
-    cardElement.remove();
-  }
   
